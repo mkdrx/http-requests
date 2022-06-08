0bd5c354c2f4ea55d748a5c10ab492f013979a40
Markus D <markusdrx@gmail.com>
2022-06-08 19:47:19 +0200

    sending first GET request

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
+  // State updating function
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = () => {
+    // As a string the request - its a promise accessed by then() when a response (object) is given
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        // returns another promise that will fired once the data transformation/parse is done
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+
+        setMovies(transformedMovies);
+      });
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
